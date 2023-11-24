40d1c233c7a5ccd389ddaf2a2de0ecf3c7096124
Adam <ad.kn1@icloud.com>
2023-11-24 10:31:09 -0500

    Clean up rust

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
 
-use std::fs::{write, OpenOptions, File};
+use std::fs::{OpenOptions, File};
 use std::path::Path;
 use std::io::{Error, Write};
 
 fn main() -> Result<(), Error> {
-    //let path = Path::new("/dev/hidg0");
-    let path = Path::new("test");
+    let path = Path::new("/dev/hidg0");
+    // let path = Path::new("test");
 
-    // write!(keyboard_file, "\\x03\\x00\\x00\\x10\\x00\\x00\\x00\\x00\\x00");
-    // write!(keyboard_file, "\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00");
     let mut keyboard_file: File = OpenOptions::new().append(true).open(path)?;
     keyboard_file.write(b"\\x03\\x00\\x00\\x10\\x00\\x00\\x00\\x00\\x00")?;
     keyboard_file.write( b"\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00")?;
 
-    //write(path, b"\\x03\\x00\\x00\\x10\\x00\\x00\\x00\\x00\\x00")?;
-    //write(path, b"\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00")?;
 
     Ok(())
 }
