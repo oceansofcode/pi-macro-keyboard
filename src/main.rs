bf102f629f90c51367b6b4c5ca914baa89b23de7
Adam <adam@oceansofcode.com>
2026-09-06 14:10:13 -0400

    minor optimizations of variables and const performance improvements

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,9 @@ use std::io::{Error, Write};
 
 use tiny_http::Server;
 
+const PORT: &str = "59285";
+
 fn main() -> Result<(), Error> {
-
-    const PORT: &str = "59285";
-
     let home_dir = home_dir().expect("No home");
 
     let home_dir = home_dir.display().to_string() + "/auth.txt";
@@ -18,34 +17,24 @@ fn main() -> Result<(), Error> {
     let tiny_http = Server::http("0.0.0.0:".to_string() + PORT).unwrap();
 
     for request in tiny_http.incoming_requests() {
-        let headers = request.headers();
-
-        for header in headers {
-
+        for header in request.headers() {
             if header.field.as_str().eq("X-Auth") {
-                let value = header.value.to_string();
-                let auth_check = value.trim() == auth.trim();
-
-                if auth_check {
-
-                    let url = request.url().trim();
-
-                    if url == "/teams-toggle" {
+                if header.value.to_string().trim() == auth.trim() {
+                    if request.url().trim() == "/teams-toggle" {
                         trigger_mute_unmute()?;
+                        break;
                     }
-
                 }
             }
         }
+        break;
     }
 
     Ok(())
 }
 
 fn trigger_mute_unmute () -> Result<(), Error> {
-
-    let path: &Path = Path::new("/dev/hidg0");
-    let mut keyboard_file: File = OpenOptions::new().append(true).open(path)?;
+    let mut keyboard_file: File = OpenOptions::new().append(true).open(Path::new("/dev/hidg0"))?;
 
     keyboard_file.write(b"\x03\x00\x00\x10\x00\x00\x00\x00")?;
     keyboard_file.write(b"\x00\x00\x00\x00\x00\x00\x00\x00")?;
