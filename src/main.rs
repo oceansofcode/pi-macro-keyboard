4782a3de5bc80de172df3ec7f92b6a2986e3a0fe
Adam <adam@oceansofcode.com>
2026-09-06 12:15:33 -0400

    Finish web server with basic header auth check for mute/unmute

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,42 @@
-use std::fs::{OpenOptions, File};
+use std::env::home_dir;
+use std::fs::{self, File, OpenOptions};
 use std::path::Path;
 use std::io::{Error, Write};
 
 use tiny_http::Server;
 
 fn main() -> Result<(), Error> {
+
+    let home_dir = home_dir().expect("No home?!");
+
+    const AUTH_FILE: &str = "auth.txt";
+
+    let home_dir = home_dir.display().to_string() + "/" + AUTH_FILE;
+
+    let home_dir = Path::new(&home_dir);
+
+    let auth = fs::read_to_string(home_dir).expect("No Auth File");
+
     let tiny_http = Server::http("0.0.0.0:7878").unwrap();
 
     for request in tiny_http.incoming_requests() {
         let headers = request.headers();
+
+        println!("request {request:#?}");
         
         for header in headers {
 
-            if header.field.as_str().eq("X-Auth") && header.value == "test" {
-                trigger_mute_unmute()?;
+            if header.field.as_str().eq("X-Auth") {
+                let value = header.value.to_string();
+                let auth_check = value.trim() == auth.trim();
+
+                if auth_check {
+                    println!("request!");
+                    trigger_mute_unmute()?;
+                }
             }
         }
     }
-
 
     Ok(())
 }
