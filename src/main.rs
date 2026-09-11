f8b83abb126553f823197f7509915443ba8775f5
Adam <adam@oceansofcode.com>
2026-09-11 11:20:05 -0400

    Const improvements

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,18 @@ use std::{thread, time};
 
 use tiny_http::Server;
 
+const AUTH_HEADER: &str = "X-Auth";
+const AUTH_FILE: &str = "/auth.txt";
+
 const PORT: &str = "59285";
+
+const TOGGLE_MUTE_ENDPOINT: &str = "/teams-toggle";
+const TOGGLE_END_CALL_ENDPOINT: &str = "/end-call";
 
 fn main() -> Result<(), Error> {
     let home_dir = home_dir().expect("No home");
 
-    let home_dir = home_dir.display().to_string() + "/auth.txt";
+    let home_dir = home_dir.display().to_string() + AUTH_FILE;
 
     let auth = fs::read_to_string(home_dir).expect("No Auth File");
 
@@ -19,12 +25,12 @@ fn main() -> Result<(), Error> {
 
     for request in tiny_http.incoming_requests() {
         for header in request.headers() {
-            if header.field.as_str().eq("X-Auth") {
+            if header.field.as_str().eq(AUTH_HEADER) {
                 if header.value.to_string().trim() == auth.trim() {
-                    if request.url().trim() == "/teams-toggle" {
+                    if request.url().trim() == TOGGLE_MUTE_ENDPOINT {
                         trigger_keyboard(b"\x03\x00\x10\x00\x00\x00\x00\x00")?;
                         continue;
-                    } else if request.url().trim() == "/end-call" {
+                    } else if request.url().trim() == TOGGLE_END_CALL_ENDPOINT {
                         trigger_keyboard(b"\x03\x00\x0b\x00\x00\x00\x00\x00")?;
                         continue;
                     }
